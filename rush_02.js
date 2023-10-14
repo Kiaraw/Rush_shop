6063e7334f6a6166e46673651de16251ed0a3b32
unfinishedveggies <ariane.job@epitech.digital>
2023-10-14 15:18:52 +0200

    le filter marche
    manque juste la mise en page

--- rush_02.js
+++ rush_02.js
@@ -1,28 +1,12 @@
-
- 
-
 const priceRange = document.getElementById('price-range');
-
 const priceLabel = document.getElementById('price-label');
-
- 
 
 priceRange.addEventListener('input', updatePriceLabel);
 
- 
-
 function updatePriceLabel() {
-
     const selectedPrice = priceRange.value;
-
     priceLabel.textContent = `$0 - $${selectedPrice}`;
-
     filterProducts(); // Call your filtering function here
-
 }
 
- 
-
 updatePriceLabel();
-
- 
